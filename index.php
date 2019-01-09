c3a564d0dddbfe9de1e6cbb28d48e965d5cff16a
LaetitiaMonserrat <laetitia.monserrat@ynov.com>
2019-01-09 16:11:04 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,51 +9,90 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<?php /*include_once("img/sprite_symbol_pictos.svg")*/ ?>
 	<header>
-		<span>
-			<img src="logos/EnergiesCollectives_rectangle_Logo_ajuste.svg" alt="logo Energie collectives">
-			<a id="menu" href="#"><img src="img/svg/menu.svg" alt="Logo menu"></a>
-		</span>
-		<img src="Photos/20161202_101925.jpg" alt="Photo presentation">
-		<div class="menuDesktop">
-			<nav>
-				<a href="#actualites">Actualités</a>
-				<a href="#demarche">Nous connaître</a>
-				<a href="#installations">Nos Installations</a>
-				<a href="#temoignages">Economisons</a>
-				<a href="#souscrire">En savoir plus</a>
-			</nav>
-			<a href="#souscrire" class="">Souscrire</a>			
+		<div style="width: 0;height: 0;">
+			<?php require "img/sprite_symbol_pictos.svg" ?>
 		</div>
+
+		<div class="unspan">
+			<div class="logo">
+				<img class="logo_site" src="img/logos/EnergiesCollectives_rectangle_Logo_ajuste.svg" alt="logo Energie collectives">
+			</div>
+
+			<a id="iconeMenu" href="#">
+				<svg class="IconSVG" alt="Menu">
+				<title>Menu</title>
+				<use xlink:href="#menu"/>
+				</svg>
+			</a>
+
+			<div id="menu_principal" class="displayNone">
+				<nav class="nav_menu">
+					<ul>
+						<li>
+							<a href="#actualites">Actualités</a>
+						</li>
+						<li>
+							<a href="#demarche">Nous connaître</a>
+						</li>
+						<li>
+							<a href="#installations">Nos Installations</a>
+						</li>
+						<li>
+							<a href="#temoignages">Economisons</a>
+						</li>
+						<li>
+							<a href="#souscrire">En savoir plus</a>
+						</li>
+						<li>
+							<a href="#souscrire" class="souscrire">Souscrire</a>			
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+		<img src="img/Photos/20161202_101925.jpg" alt="Photo presentation">
 
 	</header>
 	<main>
 		<section>
 			<article>
-				<img src="logos/EnergiesCollectivesLogo_ss_texte.svg" alt="logo energies collectives">
+				<img src="img/logos/EnergiesCollectivesLogo_ss_texte.svg" alt="logo energies collectives">
 				<h1>
 					L'énergie est notre affaire 
-					<div>Investissons dans la transition énérgétique</div>
+					<div>Investissons dans la transition énergétique</div>
 				</h1>
 				<div class="presentation">
 					<span class="coop">
-						<img src="img/svg/picto_coop.svg" alt="coopérateurs">
+						<svg class="" width="110px" height="110px" alt="coopérateurs">
+							<title>Coopérateurs</title>
+							<use xlink:href="#picto_coop"/>
+						</svg>
 						<p>Coopérateurs</p>
 						<p>190</p>
 					</span>
 					<span class="photov">
-						<img src="img/svg/picto_photovoltaique.svg" alt="installations">
+						<svg class="" width="110px" height="110px" alt="installations">
+							<title>installations</title>
+							<use xlink:href="#picto_photovoltaique"/>
+						</svg>
 						<p>Installations</p>
 						<p>8</p>
 					</span>
 					<span class="prod">
-						<img src="img/svg/picto_production_energie.svg" alt="production annuelle">
+						<svg class="" width="110px" height="110px" alt="production annuelle">
+							<title>production annuelle</title>
+							<use xlink:href="#picto_production_energie"/>
+						</svg>
 						<p>Production Annuelle</p>
 						<p>141 MWh</p>
 					</span>
 					<span class="capital">
-						<img src="img/svg/picto_capital.svg" alt="capital">
+						<svg class="" width="110px" height="110px" alt="capital">
+							<title>capital</title>
+							<use xlink:href="#picto_capital"/>
+						</svg>
 						<p>Capital</p>
 						<p>112 950 €</p>
 					</span>
@@ -69,9 +108,14 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<aside>
+				<aside class="btn">
 					Comprendre notre modèle de production
-					<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
+					<a href="#">
+						<svg class="" width="25px" height="25px" alt="bouton plus">
+							<title>Bouton Plus</title>
+							<use xlink:href="#plus"/>
+						</svg>
+					</a>
 				</aside>
 			</article>
 			<article class="bgdot">
@@ -84,18 +128,34 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
+				<a href="#">
+					<svg class="" width="25px" height="25px" alt="bouton plus">
+						<title>Bouton Plus</title>
+						<use xlink:href="#plus"/>
+					</svg>
+				</a>
 			</article>
-				<a href="#">Souscrire ! <img src="img/svg/schema_souscrire.svg" alt="coche"></a>
+				<a href="#" class="btn souscrire">Souscrire ! 
+					<svg class="" width="25px" height="25px" alt="valide">
+						<title>Valide</title>
+						<use xlink:href="#schema_souscrire"/>
+					</svg>
+				</a>
 		</section>
-		<section>
+		<section id="articles">
 			<article id="actualites">
 				<span>
 					<h2>Actualités</h2>
-					<img src="img/svg/fleche_gauche.svg" alt="flèche gauche">
-					<img src="img/svg/fleche_droite.svg" alt="flèche droite">
+					<svg class="" width="25px" height="25px" alt="Flèche gauche">
+						<title>Flèche gauche</title>
+						<use xlink:href="#fleche_gauche"/>
+					</svg>
+					<svg class="" width="25px" height="25px" alt="Flèche droite">
+						<title>Flèche droite</title>
+						<use xlink:href="#fleche_droite"/>
+					</svg>
 				</span>
-				<img src="Photos/IMG_20170121_122143.jpg" alt="Des gens avec les bras en l'air">
+				<img src="img/Photos/IMG_20170121_122143.jpg" alt="Des gens avec les bras en l'air">
 				<h3>Un 24 novembre de célébration</h3>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -105,11 +165,16 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
+				<a href="#">
+					<svg class="" width="25px" height="25px" alt="bouton plus">
+						<title>Bouton Plus</title>
+						<use xlink:href="#plus"/>
+					</svg>
+				</a>
 			</article>
 			<article id="demarche">
 				<h2>Notre démarche</h2>
-				<img src="Photos/IMG_4106.jpg" alt="Des gens dans une classe">
+				<img src="img/Photos/IMG_4106.jpg" alt="Des gens dans une classe">
 				<h3>Notre démarche</h3>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -119,11 +184,16 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
+				<a href="#">
+					<svg class="" width="25px" height="25px" alt="bouton plus">
+						<title>Bouton Plus</title>
+						<use xlink:href="#plus"/>
+					</svg>
+				</a>
 			</article>
 			<article id="installations">
 				<h2>Nos Installations</h2>
-				<img src="Photos/recadrage1_site.jpg" alt="une maison">
+				<img src="img/Photos/recadrage1_site.jpg" alt="une maison">
 				<h3>Atelier Burger, Embrun</h3>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -133,12 +203,21 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
-				<a href="#" title="">Voir toutes nos installations</a>
+				<a href="#">
+					<svg class="" width="25px" height="25px" alt="bouton plus">
+						<title>Bouton Plus</title>
+						<use xlink:href="#plus"/>
+					</svg>
+				</a>
+				<a href="#" class="btn">Voir toutes nos installations</a>
 			</article>
 			<article id="temoignages">
 				<h2>Témoignages</h2>
-				<img src="Photos/recadrage1_site.jpg" alt="une maison">
+				<svg class="" width="25px" height="25px" alt="Flèche droite">
+					<title>Flèche droite</title>
+					<use xlink:href="#fleche_droite"/>
+				</svg>
+				<img src="img/Photos/recadrage1_site.jpg" alt="une maison">
 				<h3>Mairie de Puy-Sanières</h3>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -148,41 +227,67 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<a href="#"><img src="img/svg/plus.svg" alt="bouton plus"></a>
+				<a href="#">
+					<svg class="" width="25px" height="25px" alt="bouton plus">
+						<title>Bouton Plus</title>
+						<use xlink:href="#plus"/>
+					</svg>
+				</a>
 			</article>
 		</section>
 	</main>
 	<footer>
-		<a href="#">Nous suivre <img src="img/svg/facebook-square-social-logo.svg" alt="logo facebook"></a>
-		<a href="#">Nous contacter <img src="img/svg/mail.svg" alt="boite mail"></a>
-		<a href="#">Rechercher <img src="img/svg/search.svg" alt="loupe"></a>
+		<a href="#">Nous suivre 
+		<svg class="" width="25px" height="25px" alt="facebook">
+			<title>Réseau social</title>
+			<use xlink:href="#facebook-square-social-logo"/>
+		</svg>
+		</a>
+		<a href="#">Nous contacter 
+		<svg class="" width="25px" height="25px" alt="mail">
+			<title>Mail</title>
+			<use xlink:href="#mail"/>
+		</svg>
+		</a>
+		<a href="#">Rechercher
+		<svg class="" width="25px" height="25px" alt="rechercher">
+			<title>rechercher</title>
+			<use xlink:href="#search"/>
+		</svg>
+		</a>
 		<form id="souscrire" accept-charset="utf-8">
 			<label for="newsletter">Abonnez-vous à la Newsletter</label>
 			<input type="email" id="newsletter" name="newsletter" value="">
-			<a href="#"><img src="img/svg/schema_souscrire.svg" alt="bouton envoyer"></a>
+			<a href="#">
+			<svg class="" width="25px" height="25px" alt="souscrire">
+				<title>Souscrire</title>
+				<use xlink:href="#schema_souscrire"/>
+			</svg>
+			</a>
 		</form>
 		<aside>
 			Nos partenaires :
-			<img src="partenaires/arton110.jpg" alt="partenaire">	
-			<img src="partenaires/arton124.jpg" alt="partenaire">	
-			<img src="partenaires/arton82.jpg" alt="partenaire">		
-			<img src="partenaires/enercoop_logo.png" alt="partenaire">	
-			<img src="partenaires/energie-partagee_logo.png" alt="partenaire">	
-			<img src="partenaires/Initiative-nord-hautes-alpes.png" alt="partenaire">	
-			<img src="partenaires/nef_logo.png" alt="partenaire">	
-			<img src="partenaires/omnis_logo.png" alt="partenaire">	
-			<img src="partenaires/paca_logo.png" alt="partenaire">	
-			<img src="partenaires/parc-ecrins_logo.jpg" alt="partenaire">
-			<img src="partenaires/syme05_logo.png" alt="partenaire">	
-			<img src="partenaires/udess-05_logo-seul.png" alt="partenaire">	
+			<img src="img/partenaires/arton110.jpg" alt="partenaire">	
+			<img src="img/partenaires/arton124.jpg" alt="partenaire">	
+			<img src="img/partenaires/arton82.jpg" alt="partenaire">		
+			<img src="img/partenaires/enercoop_logo.png" alt="partenaire">	
+			<img src="img/partenaires/energie-partagee_logo.png" alt="partenaire">	
+			<img src="img/partenaires/Initiative-nord-hautes-alpes.png" alt="partenaire">	
+			<img src="img/partenaires/nef_logo.png" alt="partenaire">	
+			<img src="img/partenaires/omnis_logo.png" alt="partenaire">	
+			<img src="img/partenaires/paca_logo.png" alt="partenaire">	
+			<img src="img/partenaires/parc-ecrins_logo.jpg" alt="partenaire">
+			<img src="img/partenaires/syme05_logo.png" alt="partenaire">	
+			<img src="img/partenaires/udess-05_logo-seul.png" alt="partenaire">	
 		</aside>
 	</footer>
 	<script>
 		document.addEventListener('DOMContentLoaded', function(){
-			let menu = document.getElementById('menu');
+			let menu = document.getElementById('iconeMenu');
+			let menuCache = document.getElementById('menu_principal');
 			console.log(menu);
 			menu.addEventListener('click', function(){
-				menu.classList.toggle('menu');
+				menuCache.classList.toggle('displayM');
 			});
 		});
 	</script>
